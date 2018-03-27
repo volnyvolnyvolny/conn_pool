5ba715650327bf65bc1a0c6245dcd39c59ef8900
Valentin <flomop@ya.ru>
2018-03-27 04:41:57 +0300

    …

--- lib/conn_pool/application.ex
+++ lib/conn_pool/application.ex
@@ -1,20 +1,20 @@
 defmodule ConnPool.Application do
   # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
+  # for more information on OTP Applications.
   @moduledoc false
 
   use Application
 
   def start(_type, _args) do
-    # List all child processes to be supervised
+    # List all child processes to be supervised.
     children = [
-      # Starts a worker by calling: ConnPool.Worker.start_link(arg)
-      # {ConnPool.Worker, arg},
+      # Starts pool by calling: Conn.Pool.start_link([]).
+      {Conn.Pool, []}
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
-    opts = [strategy: :one_for_one, name: ConnPool.Supervisor]
+    opts = [strategy: :one_for_one, name: Conn.Pool.Sup]
     Supervisor.start_link(children, opts)
   end
 end
