4e59f68ff86ab068b0fe17b102c4e95b19f96643
Valentin Vasilev <flomop@ya.ru>
2018-04-18 16:52:15 +0300

    fix mix.exs

--- mix.exs
+++ mix.exs
@@ -6,6 +6,12 @@ defmodule ConnPool.Mixfile do
       app: :conn_pool,
       name: "Conn.Pool",
       version: "0.2.1",
+      description: """
+        Connection pool helps storing, sharing and using connections. It also make its possible to use the 
+        same connection concurrently. For example, if there exists remote API accessible via websocket,
+        pool can provide shared access, making queues of calls. If connection is closed/expires — pool
+        will reinitialize or drop it and awaiting calls will be moved to another connection queue.
+      """,
       elixir: "~> 1.6",
       deps: deps(),
       docs: docs(),
